4fbed9b786e5e1797238a0731bf5192be4cc0486
x10102 <x10102.ot@gmail.com>
2026-07-24 01:50:57 +0200

    Write better docstrings for ModuleBase methods

--- core/modulebase.py
+++ core/modulebase.py
@@ -2,11 +2,16 @@ from discord import Cog
 from logging import info
 
 class ModuleBase(Cog):
+
+    def __init__(self):
+        super().__init__()
     
     @staticmethod
     def env_override() -> str:
         """
-        Returns the name of the environment variable that will prevent the module from loading if set to 'true'
+        Returns the name of the config variable that will prevent the module from loading if set to 'true'
+
+        Despite the name, this does not have to be an environment variable; It may be present in the config "overrides" section as well.
         """
         return ""
     
@@ -25,10 +30,20 @@ class ModuleBase(Cog):
         return []
     
     def print_config(self) -> list[str]:
+        """
+        Returns the module's configuration as a list of lines to be sent/printed.
+
+        It's done this way so that the formatting can be consistent across all modules, for this reason, the lines shouldn't contain any formatting or newlines.
+        """
         return ["Modul neohlásil žádnou konfiguraci"]
     
     def __str__(self):
         return f"{self.name()} Module, requires config: [{"".join(self.config_required)}]"
     
     def cog_unload(self) -> None:
+        """
+        A callback for when the module unloads. This runs before each restart of the bot currently.
+
+        Overrides discord.Cog's cog_unload
+        """
         info(f"Module {self.name()} has unloaded")
