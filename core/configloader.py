ffcde9a825037ad069dfb14399654badd4ca5784
x10102 <x10102.ot@gmail.com>
2026-07-24 01:49:11 +0200

    Document this better, might actually be reusable for future projects

--- core/configloader.py
+++ core/configloader.py
@@ -1,5 +1,6 @@
 import json
 import os
+import copy
 from logging import error
 from dotenv import load_dotenv
 from typing import Any, TypeVar, cast
@@ -54,6 +55,11 @@ class ConfigLoader():
             return default
         
     def set(self, key: str, value: Any) -> None:
+        """
+        Sets a key in the config to a value, dot syntax may be used for nested keys. 
+        All nest levels will be created if they don't exist.
+        See get() docstring for details.
+        """
         keys = key.split('.')
         key_count = len(keys)
         if key_count == 1:
@@ -69,6 +75,19 @@ class ConfigLoader():
             current = current[k]
 
     def get(self, key: str, default: T | None = None) -> T | None:
+        """
+        Retrieves a key from the config. Keys in nested dicts can be accessed using the dot syntax:
+        ```
+        config.load_from_dict(
+            {"a": {
+                "b": {
+                    "c": "d"}}})
+
+        print(config.get("a.b.c")) # "d"
+        ```
+
+        Second parameter sets the default value, analogous to python's .get()
+        """
         keys = key.split('.')
         if len(keys) == 1:
             if key in self._config:
@@ -92,19 +111,32 @@ class ConfigLoader():
         return result
     
     def scope(self, key: str) -> ScopedConfigLoader:
+        """
+        Returns a ConfigLoader which prefixes every get()/set() with the given key.
+        """
         return ScopedConfigLoader(key, self)
     
     def keys_missing(self, keys: list[str]) -> list[str]:
+        """
+        Returns a list of keys which are present in the passed list but not in config.
+        """
         if keys is None:
             return []
         return [k for k in keys if self.get(k) is None]
 
-    def load_from_env(self) -> bool:
-        load_result = load_dotenv(override=True, verbose=True, encoding='utf-8')
+    def load_from_env(self, *, override=True) -> bool:
+        """
+        Loads config from a .env file. Deprecated, loading from a .json file is preferred.
+        WILL override OS environment variables with values present in the file by default
+        """
+        load_result = load_dotenv(override=override, verbose=True, encoding='utf-8')
         self._config.update(os.environ)
         return load_result
 
     def load_from_json(self, path: DescriptorOrPath = 'config.json') -> bool:
+        """
+        Loads config from a json file. Returns True on success and False on failure. Does not raise exceptions.
+        """
         try:
             with open(path, 'r', encoding='utf-8') as configfile:
                 self._config.update(json.load(configfile))
@@ -115,6 +147,12 @@ class ConfigLoader():
             error(f"Failed to load config: JSON decode error ({str(e)})")
             return False
         return True
+
+    def load_from_dict(self, config: dict[str, Any]) -> None:
+        """
+        Loads config from a dict object. The passed dict is deep-copied, modifying the config will not modify it.
+        """
+        self._config = copy.deepcopy(config)
 
     def write_to_json(self, path: DescriptorOrPath = 'config.json') -> None:
         """
