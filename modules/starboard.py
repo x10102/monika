760a23912b21e3cb01860a3a0a854371e0191591
x10102 <x10102.ot@gmail.com>
2026-08-30 18:35:19 +0200

    Document the starboard migration method

--- modules/starboard.py
+++ modules/starboard.py
@@ -55,31 +55,52 @@ class StarboardModule(ModuleBase):
     @discord.default_permissions(administrator=True)
     @discord.slash_command(name='fsbmid', description="Doplní chybějící informace do Starboard tabulky, prosím nepoužívat")
     async def fetch_starboard_message_ids(self, ctx: discord.ApplicationContext):
+        # This is effectively a migration for the new Starboard table because sadly I don't have a proper migration system set up
+
+        # This will definitely take longer than 3 seconds
         await ctx.defer()
+
+        # Get the channel, make MyPy happy and loop over the messages
         starboard_channel = cast(discord.abc.Messageable, await self.bot.fetch_channel(self.channel))
         async for pinned_msg in starboard_channel.history():
+
+            # Grab the first embed from every starboard message
             embed = pinned_msg.embeds[0]
             if not embed:
                 info("No embed")
                 continue
             url_field = embed.fields[1].value
-            # I hate regex and I refuse to use it here
-            # Slice footer with the message link into the message ID and channel ID
+
+            # The footer contains a link to the original message, which we can use to grab the message ID and channel ID
             url_split = url_field.split('/')
             message_id = int(url_split[-1][:-3])
             channel_id = int(url_split[-2])
+
+            # Get the database record for the message
             msg_model: StarboardPinnedMessage = StarboardPinnedMessage.get_or_none((StarboardPinnedMessage.message_id == message_id) 
                                                            & (StarboardPinnedMessage.pinned_at.is_null(False)))
             if not msg_model:
                 info(f"ID {message_id} No model")
                 continue
+
+            # Update the starboard_id field with the embed's message ID
             msg_model.starboard_id = pinned_msg.id
             msg_model.save()
             info(f"SB {pinned_msg.id} => {message_id}")
+
+            # Grab the original message
             target_channel = cast(discord.TextChannel, await self.bot.fetch_channel(channel_id))
             target_message = await target_channel.fetch_message(message_id)
+
+            # Loop over the reactions
             for reaction in target_message.reactions:
-                message_react_partial = discord.PartialEmoji.from_str(reaction.emoji) if isinstance(reaction.emoji, str) else reaction.emoji if isinstance(reaction.emoji, discord.PartialEmoji) else reaction.emoji._to_partial()
+
+                # Use this horrible thing to convert the reaction to a PartialEmoji, which we can compare to our monitored emoji list
+                message_react_partial = discord.PartialEmoji.from_str(reaction.emoji) if isinstance(reaction.emoji, str)\
+                                        else reaction.emoji if isinstance(reaction.emoji, discord.PartialEmoji)\
+                                        else reaction.emoji._to_partial()
+
+                # If they match, set the new reaction count in database and call edit_starboard_pin() on the record to update the embed
                 if message_react_partial == discord.PartialEmoji.from_str(msg_model.emoji) and reaction.count > msg_model.reaction_count:
                     info(f"UPDATE SB {pinned_msg.id} react x{msg_model.reaction_count} => x{reaction.count}")
                     msg_model.reaction_count = msg_model.max_reaction_count = reaction.count
@@ -87,6 +108,7 @@ class StarboardModule(ModuleBase):
                     await self.edit_starboard_pin(msg_model)
                     break
 
+        # Just say something to make the "Application is thinking" message go away
         await ctx.interaction.followup.send("done")
 
     def __init__(self, bot: MonikaBot):
