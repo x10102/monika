edd05b3fc366c4f9f3fc0c0de6c3e7a75ba6f9df
x10102 <x10102.ot@gmail.com>
2026-08-30 17:05:11 +0200

    Starboard messages update when more reactions are added after pinning, also document the module a bit better

--- modules/starboard.py
+++ modules/starboard.py
@@ -75,6 +75,22 @@ class StarboardModule(ModuleBase):
         else:
             warning("Expiration time for starboard records is not set, this might make the database grow very fast in large servers!")
 
+    async def edit_starboard_pin(self, msg: StarboardPinnedMessage):
+        if not msg.starboard_id:
+            raise RuntimeError("Editing message record with no ID")
+
+        starboard_channel = cast(discord.TextChannel, await self.bot.fetch_channel(self.channel))
+        message = await starboard_channel.fetch_message(msg.starboard_id)
+
+        info(f"Edit starboard embed ID {msg.starboard_id} for message {msg.message_id} with {msg.reaction_count} reactions")
+
+        # We'd have to store the channel ID or pass it to the function to get the original channel
+        # So we just do a bit of awful string splicing instead
+        content_slice = message.content[message.content.find('x'):]
+        new_content = f"**{msg.reaction_count}" + content_slice
+
+        await message.edit(embeds=message.embeds, content=new_content)
+
     @ModuleBase.listener()
     async def on_raw_reaction_add(self, payload: discord.RawReactionActionEvent):
         # Ignore emojis not used for stars, ignore the starboard channel and also all excluded channels
@@ -84,21 +100,57 @@ class StarboardModule(ModuleBase):
             return
         if payload.channel_id in self.excluded:
             return
+
+        # Check if the message is already pinned on the starboard with any emoji
         already_pinned = StarboardPinnedMessage.select().where((StarboardPinnedMessage.pinned_at.is_null(False))\
                                                                & (StarboardPinnedMessage.message_id == payload.message_id)).exists()
         if already_pinned:
+            msg = StarboardPinnedMessage.get_or_none(message_id = payload.message_id,
+                                        emoji = payload.emoji)
+            
+            if not msg or msg.pinned_at.is_null(True):
+                # The reaction has a different emoji than the starboard pin
+                # Just ignore it so I can keep my sanity
+                return
+            
+            msg.reaction_count += 1
+
+            # If this is the new highest reaction count, edit the starboard message for it
+            # We have to do this check since someone could spam add/remove reactions and overload the API with constant edits
+            
+            if msg.reaction_count > msg.max_reaction_count:
+                await self.edit_starboard_pin(msg)
+                msg.max_reaction_count = msg.reaction_count
+
+            # Then save it and return
+
+            msg.save()
             return
+
+        # Retrieve the row, add one reaction, update the max count
         message_model: StarboardPinnedMessage = \
             StarboardPinnedMessage.get_or_create(message_id = payload.message_id,
                                                 emoji = payload.emoji)[0]
+        
         message_model.reaction_count += 1
+        message_model.max_reaction_count = max(message_model.max_reaction_count, message_model.reaction_count)
+
+        # TODO: This is probably redundant
         if message_model.pinned_at is not None:
             return
+
         if message_model.reaction_count < self.threshold:
+            # Still under the threshold, keep the count and wait
             message_model.save()
             return
+
+        # Over the threshold
         message_model.pinned_at = datetime.now()
-        message_model.save()
+
+        # Make sure that the starboard channel is a text channel
+        # And that the source channel is either a text channel or a thread
+        # I'm not quite sure what happens when we get here with one of the voice-attached text channels
+        # But nobody uses those anyway
         channel = await self.bot.fetch_channel(payload.channel_id)
         starboard_channel = await self.bot.fetch_channel(self.channel)
         if not isinstance(channel, (discord.TextChannel, discord.Thread)) or not isinstance(starboard_channel, discord.TextChannel):
@@ -106,9 +158,12 @@ class StarboardModule(ModuleBase):
             console = cast(discord.abc.Messageable, self.bot.get_channel(self.console))
             await console.send("Pokus o načtení špatného typu kanálu v on_raw_reaction_add (TOHLE JE DEFINITIVNĚ BUG)")
             return
-        message = await channel.fetch_message(payload.message_id)
-        
 
+        # Grab the message and log it
+        message = await channel.fetch_message(payload.message_id)
+        info(f"Pin message {payload.message_id} to starboard with {message_model.reaction_count} reactions")
+
+        # Build the embed, fuck around with any potential attachments for a little
         star_embed = discord.Embed()
         star_embed.set_author(name=f"{message.author.display_name}", icon_url=message.author.display_avatar.url)
         star_embed.add_field(name="",
@@ -143,11 +198,13 @@ class StarboardModule(ModuleBase):
                     star_embed.set_image(url=embed.thumbnail.url)
                     break
 
+        # Finally make the text content for the embed, send the message, and save everything to the database
         channel_and_count = f"**{self.threshold}x {payload.emoji} v <#{message.channel.id}>**"
 
-        await starboard_channel.send(embed=star_embed, content=channel_and_count)
+        starboard_id = (await starboard_channel.send(embed=star_embed, content=channel_and_count)).id
+        message_model.starboard_id = starboard_id
+        message_model.save()
 
-        
     @ModuleBase.listener()
     async def on_raw_reaction_remove(self, payload: discord.RawReactionActionEvent):
         if payload.emoji not in self.emoji:
@@ -187,5 +244,6 @@ class StarboardPinnedMessage(ModelBase):
     emoji = CharField(64)
     pinned_at = TimestampField(null=True, default=None)
     reaction_count = IntegerField(default=0)
+    max_reaction_count = IntegerField(default=0)
     created_at = TimestampField(default=datetime.now)
-    #starboard_id = CharField(15, null=True, default=None)
+    starboard_id = CharField(15, null=True, default=None)
